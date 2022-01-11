4fe71f1c5c6e4ab45f0503a2b12f3acce11f5e95
rafy <jinhlee19@gmail.com>
2022-01-11 14:21:41 +0900

    Navigate with named Routes +

--- flash-chat/lib/main.dart
+++ flash-chat/lib/main.dart
@@ -18,8 +18,9 @@ class FlashChat extends StatelessWidget {
       home: WelcomeScreen(),
       initialRoute: 'login_screen',
       routes: {
-        'welcome_screen': (context) => WelcomeScreen(),
-        'login_screen': (context) => LoginScreen(),
+        // WelcomeScreen().id: (context) => WelcomeScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
         'registration_screen': (context) => RegistrationScreen(),
         'chat_screen': (context) => ChatScreen(),
       },
