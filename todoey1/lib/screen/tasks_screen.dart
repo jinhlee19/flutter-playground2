fab77b42f1c2c51f09f233bae4a61713e65be2d2
rafy <jinhlee19@gmail.com>
2022-01-18 08:07:56 +0900

    Todoey - Design 2

--- todoey1/lib/screen/tasks_screen.dart
+++ todoey1/lib/screen/tasks_screen.dart
@@ -5,49 +5,60 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const <Widget>[
-            CircleAvatar(
-              child: Icon(
-                Icons.list,
-                size: 30,
-                color: Colors.lightBlueAccent,
-              ),
-              backgroundColor: Colors.white,
-              radius: 30,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 30,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Tastks',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'Todoey',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 45.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Text(
-              '12 Tastks',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            const Container(
-              height: 300.0,
+          ),
+          Expanded(
+            child: Container(
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
-                  topLeft: 20,
-                  topRight: 20,
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
