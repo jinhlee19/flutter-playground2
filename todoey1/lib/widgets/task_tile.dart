032124d99de47ac59d7271ec13d1646854543e20
rafy <jinhlee19@gmail.com>
2022-01-18 17:14:16 +0900

    Checkbox - State 2 - Err

--- todoey1/lib/widgets/task_tile.dart
+++ todoey1/lib/widgets/task_tile.dart
@@ -1,32 +1,48 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  // 변수 - 변경될수 있음.
+  bool isChecked = false;
+  void checkboxCallback(bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
-      trailing: TaskCheckBox(),
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: TaskCheckbox(
+          checkboxState: isChecked, toggleCheckboxState: checkboxCallback),
     );
   }
 }
 
-class TaskCheckBox extends StatefulWidget {
-  @override
-  _TaskCheckBoxState createState() => _TaskCheckBoxState();
-}
+class TaskCheckbox extends StatelessWidget {
+  //stateless widget에서는 final -> (1) 빌드 후 값을 변경하지 않음. 변경될시엔 다시시작시켜서 (2) 지우고 리빌드
+  final bool checkboxState;
+  // final Function toggleCheckboxState;
+  final void Function(bool?) toggleCheckboxState;
+  // (2) TaskCheckbox가 실행되면 bool값을 받아서 widget이 rebuild 된다.
+  TaskCheckbox(
+      {required this.checkboxState, required this.toggleCheckboxState});
 
-class _TaskCheckBoxState extends State<TaskCheckBox> {
-  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
-      value: isChecked,
-      onChanged: (newValue) {
-        setState(() {
-          isChecked = newValue!;
-        });
-      },
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
