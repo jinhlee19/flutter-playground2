27f5a0eec784b0bf7c2ffe4e588f5a3fe9f94dee
rafy <jinhlee19@gmail.com>
2022-01-19 20:53:33 +0900

    Provider and ChangeNotifier 3

--- todoey1/lib/widgets/tasks_list.dart
+++ todoey1/lib/widgets/tasks_list.dart
@@ -6,18 +6,22 @@ import 'package:provider/provider.dart';
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-            taskTitle: Provider.of<TaskData>(context).tasks[index].name,
-            isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-            checkboxCallback: (bool? checkboxState) {
-              // setState(() {
-              //   widget.tasks[index].toggleDone();
-              // });
-            });
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+                taskTitle: Provider.of<TaskData>(context).tasks[index].name,
+                isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
+                checkboxCallback: (bool? checkboxState) {
+                  // setState(() {
+                  //   widget.tasks[index].toggleDone();
+                  // });
+                });
+          },
+          itemCount: Provider.of<TaskData>(context).tasks.length,
+        );
       },
-      itemCount: Provider.of<TaskData>(context).tasks.length,
     );
   }
 }
